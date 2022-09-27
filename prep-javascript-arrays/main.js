641251b780833415cc2df6c6c5e79514bba97c08
Michael Geraghty <Mageraghty01@gmail.com>
2022-09-26 22:37:19 -0700

    completed prep-javascript-arrays exercise.

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,10 @@
+const array_name=["html","css","javascript","react","node","postgres"]
+console.log(array_name)
+var firstElement= "html"
+console.log(firstElement)
+var thirdelement= "javascript"
+console.log(thirdelement)
+var length = 6
+console.log(length)
+var lastelement= "postgres"
+console.log(lastelement)
