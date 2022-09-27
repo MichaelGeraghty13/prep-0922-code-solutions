8f5c37b738f1a94131b1d13c576d9eda5393ceeb
Michael Geraghty <Mageraghty01@gmail.com>
2022-09-26 19:46:09 -0700

    completed prep-javascript-objects.

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,9 @@
+var person = { Firstname : "Walter", LastName: "White", Hobby: "Meth"}
+console.log(person)
+var fullName = "Walter White"
+console.log(fullName)
+var job ="Drug Maker"
+console.log(job)
+var previousJob ="Teacher"
+console.log(previousJob)
+console.log(person,fullName,job,previousJob)
