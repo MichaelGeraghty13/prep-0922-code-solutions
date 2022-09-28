336e08b397fd40cbb8ddc7ceae1c0ab6610d2021
Michael Geraghty <Mageraghty01@gmail.com>
2022-09-28 14:47:00 -0700

    completed prep-javascript-objects w changes.

--- prep-javascript-objects/main.js
+++ prep-javascript-objects/main.js
@@ -1,9 +1,19 @@
-var person = { Firstname : "Walter", LastName: "White", Hobby: "Meth"}
-console.log(person)
-var fullName = "Walter White"
-console.log(fullName)
-var job ="Drug Maker"
-console.log(job)
-var previousJob ="Teacher"
-console.log(previousJob)
-console.log(person,fullName,job,previousJob)
+var walter = {
+  firstname : 'Walter',
+  lastName: 'White',
+  Hobby: 'Meth'
+  };
+
+  var fullName = walter.firstname + ' ' + walter.lastName;
+
+  console.log("The person's name is: ", fullName +'.');
+
+  walter.job ='Drug cook';
+
+  console.log("The person's current job is: " + walter.job +'.');
+
+  walter.previousjob = 'teacher';
+
+  console.log("The person's previous job id: " + walter.previousjob +'.');
+
+  console.log('The complete person object: ', walter)
